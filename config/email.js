1e6ec908b61dd384d5a91f417bbad911e0da6ac5
Alessandro <asovitorio@gmail.com>
2020-07-15 15:33:18 -0300

    ajuste do smtp do email

--- config/email.js
+++ config/email.js
@@ -1,8 +1,10 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config()
 const email = nodemailer.createTransport({
-    service:'gmail',
-    auth:{
+   host:"smtp.gmail.com",
+   port:"465",
+   secure:"false",
+     auth:{
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASS
     }
