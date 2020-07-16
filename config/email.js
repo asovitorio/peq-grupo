8752a1adb60ee4a4d06aafbdad531d87fcc8e04e
Alessandro <asovitorio@gmail.com>
2020-07-15 23:45:44 -0300

    voltando as configurações do servidor

--- config/email.js
+++ config/email.js
@@ -1,10 +1,8 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config()
 const email = nodemailer.createTransport({
-   host:"smtp.gmail.com",
-   port:"465",
-   secure:"false",
-     auth:{
+    service:'gmail',
+    auth:{
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASS
     }
